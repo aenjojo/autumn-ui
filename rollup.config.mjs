a48ab0aac575c551782381104c73bbf5eb13fc16
Aen Jojo <josuafernando999@gmail.com>
2023-04-13 22:29:53 +0700

    chore: maintain css and peer deps when bundling

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,32 +3,39 @@ import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
+import peerDepsExt from 'rollup-plugin-peer-deps-external';
 import packageJson from './package.json' assert { type: 'json' };
 
 export default [
 	{
 		input: 'src/index.ts',
-		output: [
-			{
-				file: packageJson.main,
-				format: 'esm',
-				sourcemap: true,
-			}, {
-				file: 'dist/index.min.js',
-				format: 'iife',
-				name: 'autumn',
-				plugins: [
-					terser(),
-				],
-				sourcemap: true,
-			},
-		],
+		output: {
+			file: packageJson.main,
+			format: 'esm',
+			sourcemap: true,
+		},
 		plugins: [
+			peerDepsExt(),
 			resolve(),
 			commonjs(),
 			typescript({
 				tsconfig: './tsconfig.json',
 			}),
+			postcss({
+				config: {
+					path: './postcss.config.js',
+				},
+				extensions: [
+					'.css',
+				],
+				minimize: true,
+				extract: 'main.css',
+			}),
+			terser(),
+		],
+		external: [
+			'react-dom',
 		],
 	},
 	{
@@ -41,6 +48,9 @@ export default [
 		],
     	plugins: [
 			dts(),
+		],
+		external: [
+			/\.css$/,
 		],
 	},
 ];
