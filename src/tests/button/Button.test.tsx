18321d119fe7569c0c5521eae1c1af09f9484409
Aen Jojo <josuafernando999@gmail.com>
2023-04-24 00:56:58 +0700

    test: add test for disabled button

--- src/tests/button/Button.test.tsx
+++ src/tests/button/Button.test.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Button } from '#components';
 
 describe('Button', () => {
 	test('renders the button', () => {
-		render(<Button label='Hello' />);
+		render(
+			<Button label='Hello' />
+		);
 	});
 
 	test('renders the label', () => {
-		render(<Button label='Text' />).getByText('Text');
+		render(
+			<Button label='Text' />
+		).getByText('Text');
+	});
+
+	test('can not clicks the button', () => {
+		const handleClick = jest.fn();
+
+		render(
+			<Button
+				label='Disabled'
+				onClick={handleClick}
+				disabled
+			/>
+		);
+
+		fireEvent.click(screen.getByText(/disabled/i));
+
+		expect(handleClick).toHaveBeenCalledTimes(0);
 	});
 });
